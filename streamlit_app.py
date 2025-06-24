7ddb8de533cdeeb541ae67c1a1ff07fad283cab2
Umut Sagir <49876701+usag1r@users.noreply.github.com>
2025-06-24 20:26:40 +0000

    nggh

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,15 @@
-import os
 import streamlit as st
 import pandas as pd
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
-import os
-import csv
+import os, csv
 from PIL import Image
 from torch.utils.data import Subset
 import torch.nn as nn
 
 from tqdm import tqdm
-import torch
 from torch.utils.data import IterableDataset, DataLoader, get_worker_info
 import torch.nn.functional as F
 from torchvision.transforms import ToTensor
@@ -22,35 +19,7 @@ from model import build_model  # Import the function you wrapped in model.py
 
 model = build_model()
 
-
-
 print(model)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # ---------------------
